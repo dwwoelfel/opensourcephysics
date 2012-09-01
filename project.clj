a9b0f6dc5543996faa7ac21cd869bfe3e655202b
daniel <dwwoelfel@gmail.com>
2012-08-31 21:54:01 -0700

    adds project.clj, ready for deploy to clojars

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.clojars.dwwoelfel/osp "2.3"
+  :description "Extensive resources for computational physics and physics simulations."
+  :url "http://www.compadre.org/osp/"
+  :license {:name "GNU General Public License"
+            :url "http://www.gnu.org/copyleft/gpl.html"})
+  
